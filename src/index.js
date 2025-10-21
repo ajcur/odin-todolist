a77095471740f3e0658bfca3b6f1c1159a2a3199
ajcur <173679835+ajcur@users.noreply.github.com>
2025-10-21 13:48:00 +0200

    First attempt at implementing DOM display of to-dos

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import "./styles.css";
 import { defaultProject, Project, projectList } from "./projects.js";
 import { allToDos, ToDo } from "./todo.js";
+import { renderToDoDisplay } from "./displayToDo.js";
 
-new ToDo("toDo1", 0, new Date(2025, 10, 14));
-new ToDo("toDo2", 0, new Date());
+renderToDoDisplay(new ToDo("To-Do 1", 0, new Date(2025, 10, 14)));
+renderToDoDisplay(new ToDo("toDo2", 0, new Date(), "high"));
 new ToDo("toDo3", 0, new Date());
 
 new Project("project1", 0, 0);
 new Project("project2", 0, 0);
 
-projectList[0].addToDo(allToDos[0]);
+projectList[1].addToDo(allToDos[0]);
 /* project1.addToDo(toDo2);
 project2.addToDo(toDo3); */
 
